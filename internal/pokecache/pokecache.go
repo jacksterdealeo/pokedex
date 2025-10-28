41a91f4e3d45b147a4f18a0c4d3aff13c2b57725
jacksterdealeo <oswaldtechnic@zoho.com>
2025-10-28 00:05:31 -0800

    worked on caching

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,56 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	mu      sync.Mutex
+	entries map[string](*cacheEntry)
+}
+
+func NewCache(interval time.Duration) *Cache {
+	spawnedCashe := Cache{
+		mu:      sync.Mutex{},
+		entries: make(map[string](*cacheEntry)),
+	}
+	spawnedCashe.reapLoop(time.Duration(time.Second) * 5)
+	return &spawnedCashe
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.entries[key] = &cacheEntry{time.Now(), val}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	result := c.entries[key]
+	if result == nil {
+		return []byte{}, false
+	}
+	return (*result).val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	go func() {
+		for {
+			time.Sleep(interval)
+			c.mu.Lock()
+			for key, val := range c.entries {
+				if time.Since(val.createdAt) > interval {
+					delete(c.entries, key)
+				}
+			}
+			c.mu.Unlock()
+		}
+	}()
+}
